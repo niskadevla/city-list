7750fd840d9767ed428608746281d20ead863915
Denis <niskadevla@gmail.com>
2020-10-15 11:39:17 +0300

    Created integration test for AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,18 @@
 import { AppComponent } from './app.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SelectorComponent } from './features/selector/selector.component';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { ChangeDetectorRef, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { CommonModule } from '@angular/common';
+import { SelectorModule } from './features/selector/selector.module';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { BrowserTestingModule } from '@angular/platform-browser/testing';
+import { By } from '@angular/platform-browser';
+import { AppModule } from './app.module';
+
+
 
 describe('AppComponent', () => {
     let app: AppComponent;
@@ -9,5 +23,32 @@ describe('AppComponent', () => {
 
     it('should create the app', () => {
       expect(app).toBeTruthy();
+    });
+});
+
+describe('Integration tests. AppComponent', () => {
+    let app: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [ AppComponent ],
+            imports: [ReactiveFormsModule,
+                      BrowserTestingModule],
+            schemas: [NO_ERRORS_SCHEMA]
+        });
+
+        fixture = TestBed.createComponent(AppComponent);
+        app = fixture.debugElement.componentInstance;
+    });
+
+    it('should create the app', () => {
+        expect(app).toBeDefined();
+    });
+
+    xit('should have app-selector directive', () => {
+        const de = fixture.debugElement.query(By.directive(SelectorComponent));
+
+        expect(de).not.toBeNull();
     });
 });
--- src/app/features/selector/selector.component.spec.ts
+++ src/app/features/selector/selector.component.spec.ts
@@ -1,22 +1,27 @@
 import { SelectorComponent } from './selector.component';
-import { FormBuilder } from '@angular/forms';
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { ChangeDetectorRef, Provider } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { ICountryState } from '../../reducers/countries/countries.reducer';
+
+const STORE: Readonly<Provider> = {
+    provide: 'STORE', useValue: [
+        { code: 'AF', name: 'Afghanistan', selected: false },
+        { code: 'AL', name: 'Albania', selected: true }
+    ]
+};
 
 describe('SelectorComponent', () => {
     let component: SelectorComponent;
-    // let fixture: ComponentFixture<SelectorComponent>;
 
     beforeEach(() => {
         component = new SelectorComponent(new FormBuilder(), null, null, null);
-        // TestBed.configureTestingModule({
-        //     declarations: [SelectorComponent],
-        //     providers: [  ]
-        // });
-        // fixture = TestBed.createComponent(SelectorComponent);
-        // component = fixture.componentInstance;
+
         component.countries = [
             { code: 'AF', name: 'Afghanistan', selected: false },
-            { code: 'AL', name: 'Albania', selected: true },
+            { code: 'AL', name: 'Albania', selected: true }
         ];
     });
 
@@ -26,23 +31,66 @@ describe('SelectorComponent', () => {
     });
 
     it('should create form with 3 controls', () => {
-        spyOn(component, 'ngOnInit').and.callFake(() => {
-            expect(component.form.contains('radioGroup')).toBeTruthy();
-            expect(component.form.contains('search')).toBeTruthy();
-            expect(component.form.contains('result')).toBeTruthy();
-        });
+        spyOn(component, 'ngOnInit')
+            .and
+            .callFake(() => {
+                expect(component.form.contains('radioGroup'))
+                    .toBeTruthy();
+                expect(component.form.contains('search'))
+                    .toBeTruthy();
+                expect(component.form.contains('result'))
+                    .toBeTruthy();
+            });
     });
 
     it('Method getSelectedCountries return "Albania" if selected == true', () => {
-        expect(component.getSelectedCountries()).toEqual([{ code: 'AL', name: 'Albania', selected: true }]);
+        expect(component.getSelectedCountries())
+            .toEqual([ { code: 'AL', name: 'Albania', selected: true } ]);
     });
 
     it('Method clearInput should reset input value', () => {
-        spyOn(component, 'ngOnInit').and.callFake(() => {
-            component.clearInput();
-            expect(component.form.controls.search.value).toBeNull();
-        });
+        spyOn(component, 'ngOnInit')
+            .and
+            .callFake(() => {
+                component.clearInput();
+                expect(component.form.controls.search.value)
+                    .toBeNull();
+            });
     });
 
+});
+
+describe('Integration tests. SelectorComponent', () => {
+    let component: SelectorComponent;
+    let fixture: ComponentFixture<SelectorComponent>;
+    // let fb: FormBuilder;
+    // let dialog: MatDialog;
+    // let cdr: ChangeDetectorRef;
+    // let store$: Store<ICountryState>;
+
+    beforeEach(async (() => {
+        TestBed.configureTestingModule({
+            declarations: [ SelectorComponent ],
+            imports: [ReactiveFormsModule],
+            providers: [ FormBuilder,
+                         MatDialog,
+                         ChangeDetectorRef,
+                         Store ]
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(SelectorComponent);
+        component = fixture.debugElement.componentInstance;
+        // dialog = TestBed.inject(MatDialog);
+
+        component.countries = [
+            { code: 'AF', name: 'Afghanistan', selected: false },
+            { code: 'AL', name: 'Albania', selected: true }
+        ];
+    }));
+
+    xit('should be created', () => {
+        fixture.detectChanges();
+        expect(component).toBeDefined();
+    });
 });
 
